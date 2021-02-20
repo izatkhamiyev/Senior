cb0ef37158e1fbc3f65c8dd9d564dd994b8afc3a
Nurts <inurtas1999@gmail.com>
2021-02-20 18:07:03 +0600

    real size estimation

--- src/js/ratio.js
+++ src/js/ratio.js
@@ -6,7 +6,12 @@ ratio.display_data = function(pd_width, pd_height, W, canvas){
     var pd_dist = Math.sqrt(pd_px_width * pd_px_width + pd_px_height * pd_px_height);
     var fc_px_w = W * canvas.width;
     var pd_ratio = pd_dist / fc_px_w;
+
+
     document.getElementById("pd_display").innerText = pd_dist;
     document.getElementById("fw_display").innerText = fc_px_w;
     document.getElementById("ratio_display").innerText = pd_ratio;
+
+    var predicted_pd = (0.100791707 * pd_dist) + (-0.0325460870 * fc_px_w) + (89.938948 * pd_ratio) + 598.0447436762962;
+    document.getElementById("pd_real_display").innerText = predicted_pd;
 }
